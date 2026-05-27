1d79f1d601ccb724f9afee9e9d65313dab593362
EthanW7978 <231079028+EthanW7978@users.noreply.github.com>
2026-05-27 00:41:41 +0000

    Tue, May 26, 2026, 5:41 PM -07:00

--- game.py
+++ game.py
@@ -2733,8 +2733,6 @@ class GameView(arcade.View):
                 arcade.draw_text("VICTORY", 400, 394, arcade.color.WHITE, 78, anchor_x="center")
                 arcade.draw_text("CONGRATULATIONS", 402, 316, arcade.color.BLACK, 76, anchor_x="center")
                 arcade.draw_text("CONGRATULATIONS", 400, 318, arcade.color.GOLD, 74, anchor_x="center")
-                arcade.draw_text("Celebration in progress", 400, 246, arcade.color.BLACK, 16, anchor_x="center")
-                arcade.draw_text("Celebration in progress", 400, 248, arcade.color.WHITE, 14, anchor_x="center")
                 return
 
             # Draw mini-games on top of everything
