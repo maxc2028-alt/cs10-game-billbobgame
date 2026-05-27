806b399781e9a929d0a253a040b43fb93eb40421
EthanW7978 <231079028+EthanW7978@users.noreply.github.com>
2026-05-27 04:12:03 +0000

    Tue, May 26, 2026, 9:12 PM -07:00

--- game.py
+++ game.py
@@ -979,7 +979,7 @@ class GameView(arcade.View):
 
     def maybe_open_house_style_choice(self, building_index: int) -> bool:
         flags = self.house_completion_flags.get(building_index, set())
-        if {"interior", "riddle"} <= flags:
+        if "riddle" in flags and ("exterior" in flags or "interior" in flags):
             self.pending_house_style_building = building_index
             self.inside_building = building_index
             self.screen = "decorate"
